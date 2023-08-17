2e5a0cdf2f709a93d093bc58b6c3528d30678bc1
SoniaCMS <soniacristina.ti@gmail.com>
2023-08-17 16:17:08 -0300

    Java with String boot flyway

--- javaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/sonia/controllers/PersonController.java
+++ javaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/sonia/controllers/PersonController.java
@@ -18,7 +18,7 @@ import br.com.sonia.data.vo.v1.PersonVO;
 import br.com.sonia.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 	
 	@Autowired
