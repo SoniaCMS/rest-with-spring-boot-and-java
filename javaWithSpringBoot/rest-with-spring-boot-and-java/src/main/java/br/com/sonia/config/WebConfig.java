85ad07a132b509324fff491ba7fe7d727e101ed4
SoniaCMS <soniacristina.ti@gmail.com>
2023-08-19 22:10:42 -0300

    Java with String boot Hateoas

--- javaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/sonia/config/WebConfig.java
+++ javaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/sonia/config/WebConfig.java
@@ -11,22 +11,22 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
-		configurer.favorParameter(true)
+		/*	configurer.favorParameter(true)
 		      .parameterName("mediaType").ignoreAcceptHeader(true)
 		      .useRegisteredExtensionsOnly(false)
 		      .defaultContentType(MediaType.APPLICATION_JSON)
 		      .mediaType("json",MediaType.APPLICATION_JSON)
 		      .mediaType("xml",MediaType.APPLICATION_XML);
-	}
+	}*/
 	
 		
-	/*//Chamada via header
+	//Chamada via header
 	configurer.favorParameter(false)
     .ignoreAcceptHeader(false)
     .useRegisteredExtensionsOnly(false)
     .defaultContentType(MediaType.APPLICATION_JSON)
     .mediaType("json",MediaType.APPLICATION_JSON)
     .mediaType("xml",MediaType.APPLICATION_XML);
-}*/
+}
 
 }
--- javaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/sonia/services/PersonServices.java
+++ javaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/sonia/services/PersonServices.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.stereotype.Service;
 
+import br.com.sonia.controllers.PersonController;
 import br.com.sonia.data.vo.v1.PersonVO;
 import br.com.sonia.exceptions.ResourceNotFoundException;
 import br.com.sonia.mapper.DozerMapper;
@@ -33,7 +36,9 @@ public class PersonServices {
 
 		var entity = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("No records for this ID!"));
-		return DozerMapper.parseObject(entity,PersonVO.class);
+		PersonVO vo = DozerMapper.parseObject(entity,PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return vo;
 	}
 	
 	public PersonVO create(PersonVO person) {
@@ -50,7 +55,7 @@ public class PersonServices {
 		
 		logger.info("Updating one person!");
 		
-		var entity = repository.findById(person.getId())
+		var entity = repository.findById(person.getKey())
 				.orElseThrow(() -> new ResourceNotFoundException("No records for this ID!"));
 		
 		entity.setFirstName(person.getFirstName());
